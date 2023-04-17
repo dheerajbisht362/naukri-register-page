dd5b064c46ac97c738155c8c5f809500dc0eebfb
Manoj Kumar Nishad <manoj.n@inspironlabs.com>
2023-04-17 12:54:28 +0530

    register api created

--- register-page-BE/server.js
+++ register-page-BE/server.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const app = express();
-const PORT = 8000;
-app.listen(PORT,()=>{
+require("dotenv").config();
+const registerRoute = require('./routes/registerRoute');
+
+app.use("/", registerRoute)
+
+const PORT = process.env.PORT || 8000;
+
+app.listen(PORT, () => {
     console.log(`server is running on http://localhost:${PORT}`);
 })
