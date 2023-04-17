9a407918570eda5c32746dc0fc22c9afb9ea89a9
Gauri Patil <gauri.p@inspironlabs.com>
2023-04-17 18:41:05 +0530

    fr changes in progress

--- register-page-FR/src/Pages/registerPage.tsx
+++ register-page-FR/src/Pages/registerPage.tsx
@@ -1,10 +1,12 @@
 import Footer from "../Components/Register/footer"
 import RightContainer from "../Components/Register/right-cont"
+import Work from "../Components/Register/work"
 
 
 export default function RegisterPage(){
     return <div>
-        <RightContainer/>
-        <Footer />
+        {/* <RightContainer/>
+        <Footer /> */}
+        <Work/>
         </div>
 }
