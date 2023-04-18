055c5e4fba1cd385fbde04ea96d9d1ac0f76f665
Gauri Patil <gauri.p@inspironlabs.com>
2023-04-18 12:58:45 +0530

    layout created

--- register-page-FR/src/App.tsx
+++ register-page-FR/src/App.tsx
@@ -2,30 +2,21 @@ import {
     createBrowserRouter,
     RouterProvider,
   } from "react-router-dom";
-import ReactDOM from 'react-dom/client'
-import './App.css'
 import RegisterPage from "./Pages/registerPage";
 import React from "react";
-import Navbar from "./Components/Register/navbar";
 
 const router = createBrowserRouter([
     {
       path: "/register",
       element: <RegisterPage/>,
     },
-  ]);
-  
-//   ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-//     <React.StrictMode>
-//        <RouterProvider router={router} />
-//     </React.StrictMode>,
-//   )
+]);
 
 function App() {
     return (
         <React.StrictMode>
-        <RouterProvider router={router} />
-     </React.StrictMode>
+            <RouterProvider router={router} />
+        </React.StrictMode>
 )}
 
 export default App
--- register-page-FR/src/Components/header.tsx
+++ register-page-FR/src/Components/header.tsx
@@ -1,6 +1,6 @@
-import naukriLogo from "../../assets/naukri_Logo.png"
+import naukriLogo from '../assets/naukri_Logo.png'
 
-export default function RegsiterNavbar(): JSX.Element{
+export default function Header(): JSX.Element{
     return <div className="bg-whitebg flex px-20 py-2 justify-between items-center">
         <img alt="logo" src={naukriLogo} />
         <div className="text-sm">Already Registered? <span className="text-lightbluebg"> Login </span> here</div>
